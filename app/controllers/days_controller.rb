4b05f3875325bfecdfe876a3e2112f08e55101db
Jorge Nelson <jorgenelson@MacBook-Pro-van-Jorge-Nelson.local>
2016-08-01 16:24:11 +0200

    generate Days controller and add routes

---
+++ app/controllers/days_controller.rb
@@ -0,0 +1,32 @@
+class DaysController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def find_day
+    end
+
+    def day_params
+    end
+
+end
