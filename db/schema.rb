6009c9be98bbeefab2076d352cff27cfd24bd1b2
Jorge Nelson <jorgenelson@MacBook-Pro-van-Jorge-Nelson.local>
2016-08-04 21:44:49 +0200

    add user name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803085945) do
+ActiveRecord::Schema.define(version: 20160804191134) do
 
   create_table "days", force: :cascade do |t|
     t.string   "inspiration"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160803085945) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
