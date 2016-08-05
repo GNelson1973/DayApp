00376ffb0103a3ec6203389d9aab41f2215e3576
Jorge Nelson <jorgenelson@MacBook-Pro-van-Jorge-Nelson.local>
2016-08-05 15:47:59 +0200

    add picture on posts on day index page and restyled posts on index day page

--- app/helpers/days_helper.rb
+++ app/helpers/days_helper.rb
@@ -1,2 +1,11 @@
 module DaysHelper
+
+  def image_for_index(day)
+    if day.image_name.blank?
+      image_tag("default.png", class: "image_tag_listing")
+    else
+      image_tag(day.image_name, class: "image_tag_listing")
+    end
+  end
+
 end
